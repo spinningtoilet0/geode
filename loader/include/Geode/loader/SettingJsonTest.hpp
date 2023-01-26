d75a2175751af451dab8821c91b40e15384521bd
camila314 <47485054+camila314@users.noreply.github.com>
2023-01-26 14:25:51 -0600

    remove thing

--- loader/include/Geode/loader/SettingJsonTest.hpp
+++ loader/include/Geode/loader/SettingJsonTest.hpp
@@ -1,10 +1,6 @@
 #include "Setting.hpp"
 #include <json11.hpp>
 
-json11::Json to_json(const long long a) {
-    return json11::Json(static_cast<double>(a));
-}
-
 namespace geode {
     template<class T>
     bool GeodeSettingValue<T>::load(json11::Json const& json) {
